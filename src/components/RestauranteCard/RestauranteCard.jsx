fa8e0767a8dcb5271dfbda56ec1cf2bf7b8e1e7b
manoela-moyses <manoela.moyses0205@gmail.com>
2022-09-24 16:52:21 -0300

    RestaurantCard css

--- src/components/RestauranteCard/RestauranteCard.jsx
+++ src/components/RestauranteCard/RestauranteCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
+import StarIcon from '@material-ui/icons/Star';
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import "./styles.css";
+import { Box } from "@material-ui/core";
 
 export default function RestauranteCard(props) {
 
@@ -17,26 +19,29 @@ export default function RestauranteCard(props) {
                     title="Contemplative Reptile"
                 />
                 <CardContent className="conteudo">
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography gutterBottom variant="h4" component="h2">
                         {props.restaurante.nome}
                     </Typography>
                     <Typography gutterBottom variant="h5" component="h2">
                         {props.restaurante.distancia} Km
                     </Typography>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.restaurante.nota}
-                    </Typography>
+                    <Box display="flex" alignItems="center" className="div-rating">
+                        <StarIcon color="primary" style={{ fontSize: 14 }} />
+                        <Typography gutterBottom variant="h6" component="h2">
+                            {props.restaurante.nota}
+                        </Typography>
+                    </Box>
                     <Typography gutterBottom variant="h5" component="h2">
                         {props.restaurante.tempo_medio}
                     </Typography>
-                    {props.restaurante.valor_entrega?
-                    <Typography gutterBottom variant="h5" component="h2">
-                        R$ {props.restaurante.valor_entrega},00
-                    </Typography>
-                    : 
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Gratis
-                    </Typography>}
+                    {props.restaurante.valor_entrega ?
+                        <Typography gutterBottom variant="h5" component="h2">
+                            R$ {props.restaurante.valor_entrega},00
+                        </Typography>
+                        :
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Gratis
+                        </Typography>}
                 </CardContent>
             </CardActionArea>
         </Card>
